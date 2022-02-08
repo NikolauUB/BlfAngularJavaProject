a04a4a7ddf4271c3dd3ed7284a0cc5775a9f3fa4
Nikolay Barabanshchikov <nikolayub@mail.ru>
2022-02-08 16:18:55 +0300

    Update MigrateService.java

--- backend/src/main/java/wind/instrument/competitions/rest/MigrateService.java
+++ backend/src/main/java/wind/instrument/competitions/rest/MigrateService.java
@@ -154,11 +154,10 @@ public class MigrateService {
     }
 
     private String getClassicDescription() {
-        return "<p><strong>Композитор</strong>:Мишель Блавет</p>" +
-                "<p><strong>Произведение</strong>: \"Siciliana's Little Bells\" из сонаты  No. 4 in G minor \"La Lumagne\"</p>" +
-                "<p><strong>Ноты: <a href=\"http://dudari.ru/assets/score/blavet-op2-sonata-no4-ciciliana.pdf\" target=\"_blank\">Первоначальная версия</a></p>" +
-		"<p><strong>Ноты: <a href=\"http://dudari.ru/assets/score/blavet-op2-sonata-no4-ciciliana-alto.pdf\" target=\"_blank\">более удобные для альта</a></p>" +
-		"<p><strong>Исходный файл: <a href=\"http://dudari.ru/assets/score/blavet-op2-sonata-no4-ciciliana.xml\" target=\"_blank\">xml</a></p>" +
+        return "<p><strong>Композитор</strong>: </p>" +
+                "<p><strong>Произведение</strong>: </p>" +
+                "<p><strong>Ноты: <a href=\"http://dudari.ru/assets/score/Sonata_Lya_min_Valentini.tif\" target=\"_blank\">http://dudari.ru/assets/score/Sonata_Lya_min_Valentini.tif</a></p>" +
+	
                 "<p>Произведение следует играть без аккомпанемента.</p>" +
                 "<p>Удачи!</p>";
     }
